13654388f5ba7b4807a2095e134e30459e0c6dec
Jathursan1 <134722151+Jathursan1@users.noreply.github.com>
2023-06-02 10:03:42 +0800

    Add files via upload

---
+++ my creation2.py
@@ -0,0 +1,14 @@
+import turtle
+ 
+ 
+tur = turtle.Turtle()
+tur.speed(10)
+tur.color("red", "blue",)
+tur.begin_fill()
+ 
+for i in range(50):
+    tur.forward(600)
+    tur.left(170)
+ 
+tur.end_fill()
+turtle.done()
